17eb9c242aca0c1ef70ecc0066748f2f5101c226
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-11-06 10:45:02 -0500

    * began working on the Square child class * added a width and height variable to get how big the Square should be from the user

--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -1,2 +1,24 @@
+import java.awt.*;
+
 public class Square extends Shape {
+    private double width, height;
+
+    public Square(Turtle turtle, double locationX, double locationY, Color color, int boarder, double width, double height) {
+        super(turtle, locationX, locationY, color, boarder);
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public void paint() {
+
+    }
 }
