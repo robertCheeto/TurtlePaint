9fe97e55effb4725b0e842cdf6aa572a107665e7
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-11-06 12:00:55 -0500

    * working on trying to pass in parameters from createSquare() into the Square paint()

--- src/main/java/Square.java
+++ src/main/java/Square.java
@@ -1,24 +1,26 @@
 import java.awt.*;
 
 public class Square extends Shape {
-    private double width, height;
+    private int width, height;
 
-    public Square(Turtle turtle, double locationX, double locationY, Color color, int boarder, double width, double height) {
+    public Square(Turtle turtle, double locationX, double locationY, Color color, int boarder, int width, int height) {
         super(turtle, locationX, locationY, color, boarder);
         this.width = width;
         this.height = height;
     }
 
-    public double getWidth() {
+    public int getWidth() {
         return width;
     }
 
-    public double getHeight() {
+    public int getHeight() {
         return height;
     }
 
     @Override
     public void paint() {
-        World w = new World(this.width, this.height, )
+        World w = new World(this.width, this.height, this.getColor());
+        Square square = new Square(new Turtle(w), getLocationX(), getLocationY(), getColor(), getBoarder(), getWidth(), getHeight());
+
     }
 }
